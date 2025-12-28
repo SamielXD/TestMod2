378aaea562f4466ae0b5dea71dc30da5797f9834
Samiel <163655207+SamielXD@users.noreply.github.com>
2025-12-28 14:48:07 +0800

    Update TestMod.java

--- src/TestMod.java
+++ src/TestMod.java
@@ -1,10 +1,13 @@
 import arc.*;
+import arc.func.*;
 import arc.graphics.*;
 import arc.scene.ui.*;
 import arc.scene.ui.layout.*;
+import arc.struct.*;
 import arc.util.*;
 import mindustry.*;
 import mindustry.game.EventType.*;
+import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.*;
 import mindustry.ui.dialogs.*;
@@ -15,7 +18,6 @@ public class TestMod extends Mod {
     
     public TestMod() {
         Events.on(ClientLoadEvent.class, e -> {
-            // Add button to main menu
             Vars.ui.menufrag.addButton("ModInfo", Icon.info, () -> {
                 showStatsDialog();
             });
@@ -30,7 +32,6 @@ public class TestMod extends Mod {
         dialog.buttons.button("Close", dialog::hide).size(120, 50);
         dialog.show();
         
-        // Fetch stats in background
         fetchStats("SamielXD", "UnitNamerMod", stats -> {
             dialog.cont.clear();
             
@@ -49,7 +50,6 @@ public class TestMod extends Mod {
     void fetchStats(String owner, String repo, Cons<ModStats> callback) {
         Core.app.post(() -> {
             try {
-                // Fetch releases
                 String url = "https://api.github.com/repos/" + owner + "/" + repo + "/releases";
                 HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
                 conn.setRequestMethod("GET");
@@ -63,12 +63,10 @@ public class TestMod extends Mod {
                 }
                 reader.close();
                 
-                // Simple parsing (just count releases and downloads)
                 String data = response.toString();
                 int releases = countOccurrences(data, "\"tag_name\"");
                 int downloads = countDownloads(data);
                 
-                // Fetch repo info for stars
                 String repoUrl = "https://api.github.com/repos/" + owner + "/" + repo;
                 HttpURLConnection repoConn = (HttpURLConnection) new URL(repoUrl).openConnection();
                 repoConn.setRequestMethod("GET");
